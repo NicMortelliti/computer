f00945c2059006d3bcb6f6c626b27bd2722e63cd
Nic Mortelliti <npmortelliti@gmail.com>
2023-04-08 14:49:31 -0700

    add global style files

---
+++ src/styles/Global.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-weight: 400;
+}
+
+html {
+}
+
+body {
+  background: black;
+  color: white;
+}
+`;
+
+export default GlobalStyles;
