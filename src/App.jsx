9f7bc1d5373798b34623b671a060c64fbc8e2c91
Nic Mortelliti <npmortelliti@gmail.com>
2023-04-09 08:08:20 -0700

    add wink pattern

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import GlobalStyles from "./styles/Global";
 import Face from "./components/face/Face";
 
-import { blank } from "../src/data/patterns";
+import { face } from "../src/data/patterns";
 
 function App() {
   return (
     <div>
       <GlobalStyles />
-      <Face pattern={blank} />
+      <Face pattern={face.blank} />
     </div>
   );
 }
--- src/data/patterns.js
+++ src/data/patterns.js
@@ -1,11 +1,24 @@
-export const blank = [
-  [true, true, true, true, true, true, true, true, true],
-  [true, true, true, true, true, true, true, true, true],
-  [true, false, false, true, true, true, false, false, true],
-  [true, false, false, true, true, true, false, false, true],
-  [true, true, true, true, true, true, true, true, true],
-  [true, true, true, true, true, true, true, true, true],
-  [true, false, false, false, false, false, false, false, true],
-  [true, true, true, true, true, true, true, true, true],
-  [true, true, true, true, true, true, true, true, true],
-];
+export const face = {
+  blank: [
+    [true, true, true, true, true, true, true, true, true],
+    [true, true, true, true, true, true, true, true, true],
+    [true, false, false, true, true, true, false, false, true],
+    [true, false, false, true, true, true, false, false, true],
+    [true, true, true, true, true, true, true, true, true],
+    [true, true, true, true, true, true, true, true, true],
+    [true, false, false, false, false, false, false, false, true],
+    [true, true, true, true, true, true, true, true, true],
+    [true, true, true, true, true, true, true, true, true],
+  ],
+  wink: [
+    [true, true, true, true, true, true, true, true, true],
+    [true, true, true, true, true, true, true, true, true],
+    [true, true, true, true, true, true, false, false, true],
+    [true, true, true, true, true, true, false, false, true],
+    [true, true, true, true, true, true, true, true, true],
+    [true, true, true, true, true, true, true, true, true],
+    [true, false, false, false, false, false, false, false, true],
+    [true, true, true, true, true, true, true, true, true],
+    [true, true, true, true, true, true, true, true, true],
+  ],
+};
