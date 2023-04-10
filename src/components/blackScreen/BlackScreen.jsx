b8a3dc91772b4b6cd872c860cebc61bb6b886d1a
Nic Mortelliti <npmortelliti@gmail.com>
2023-04-10 14:07:37 -0700

    set up random circles

--- src/components/blackScreen/BlackScreen.jsx
+++ src/components/blackScreen/BlackScreen.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BlackScreenCont, Circle } from "../../styles/BlackScreen.style";
 
 const BlackScreen = () => {
+  const [selectedCircleIndex, setSelectedCircleIndex] = useState(null);
+  const numberOfCircles = 4;
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSelectedCircleIndex(Math.floor(Math.random() * numberOfCircles));
+    }, Math.random() * 1000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
     <BlackScreenCont>
-      <Circle bgColor="none" />
-      <Circle bgColor="none" />
-      <Circle bgColor="red" />
-      <Circle bgColor="none" />
+      {Array.from({ length: numberOfCircles }).map((_, index) => (
+        <Circle
+          key={index}
+          bgColor={index === selectedCircleIndex ? "red" : null}
+        />
+      ))}
     </BlackScreenCont>
   );
 };
